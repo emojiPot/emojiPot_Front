3b2219886f51a35c584411b1ac82006688f1949d
woorimj <128919405+woorimj@users.noreply.github.com>
2023-07-29 17:09:30 +0900

    하단 navigate 연결

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {StyleSheet, View, Text, Image, Button, Linking} from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+
+import HomeScreen from './app/screens/HomeScreen';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {View, Text, Button} from 'react-native';
+
+function HomeScreen({navigation}) {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      title: '여긴 홈 스크린입니다.',
+    });
+  });
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
