7c99325ed6adfabd30293cb589a43bffe0e7d978
woorimj <128919405+woorimj@users.noreply.github.com>
2023-08-15 21:59:12 +0900

    위치정보 + 날짜 모듈 다시 세팅

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import 'react-native-gesture-handler';
+import SearchBox from '../components/ScreenComponents/searchBox';
+import MainTopContent from '../components/ScreenComponents/mainTopContent';
+import MainBottomContent from '../components/ScreenComponents/mainBottomContent';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <SearchBox />
+      <MainTopContent />
+      <View style={styles.popularContentContainer}>
+        <Text style={styles.popularText}>인기게시물</Text>
+      </View>
+      <ScrollView style={styles.bottomContent}>
+        <MainBottomContent />
+        <MainBottomContent />
+        <MainBottomContent />
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  popularContentContainer: {
+    marginTop: 20,
+  },
+  popularText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    paddingHorizontal: 20,
+  },
+  bottomContent: {
+    paddingHorizontal: 10,
+  },
+});
+
+export default HomeScreen;
